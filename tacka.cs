526678450181b712617bdcadaa6333bcf89b622f
Ucenik <Ucenik@DESKTOP-OGGEBNA>
2026-04-01 13:44:40 +0200

    ggg

--- tacka.cs
+++ tacka.cs
@@ -19,5 +19,10 @@ namespace poligon_2026_310B
             x = a;
             y = b;
         }
+        public static bool iste(tacka A, tacka B)
+        {
+            if (( A.x == B.x) && (A.y == B.y)) return true;
+            else return false;
+        }
     }
 }       
